eaa8ca1bc4f44508e337e2d79a2ba9574ec7bba4
Ed <silva.email@yahoo.com>
2024-02-27 07:03:06 -0500

    Add dynamic rendering of notes in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,23 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+const loadNotes = (note) => {
+  return <Note 
+   key={note.id} 
+   title={note.title} 
+   content={note.content} 
+  />  
+};
 
 function App() {
+
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(loadNotes)}  
+
       <Footer />
     </div>
   );
