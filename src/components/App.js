e69056214d2bc10e8653ba1594dd4240fd39f3d8
Ed <silva.email@yahoo.com>
2024-02-27 07:20:25 -0500

    Refactor note loading in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,17 @@ import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
 
-const loadNotes = (note) => {
-  return <Note 
-   key={note.id} 
-   title={note.title} 
-   content={note.content} 
-  />  
-};
-
 function App() {
-
   return (
     <div>
       <Header />
-      {notes.map(loadNotes)} 
+      {notes.map((note) => {
+        return <Note 
+        key={note.id} 
+        title={note.title} 
+        content={note.content}           
+        />;
+      })}
       <Footer />
     </div>
   );
